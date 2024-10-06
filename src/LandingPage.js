87be0827125e89325533933730935016ea5d4ecd
Rafael Brandão <rafael@mobr.ai>
2024-10-05 23:25:23 -0300

    Improving file preview on drop

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,40 +1,34 @@
 import './LandingPage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
-
 import request from 'superagent';
 import React, { useState, useCallback } from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
 import logo from './logo.svg';
-// import placeholder from './placeholder.svg'
 import StyledDropzone from './StyledDropzone.js'
 import NavBar from './NavBar.js';
 
 function LandingPage() {
-  const welcomeMsg = "Drop files to analyze, or click to select."
+  const welcomeMsg = "Drop files here and/or enter URLs to fact-check"
+  const [disableDrop, setDisableDrop] = useState(false)
   const [dropMsg, setDropMsg] = useState(welcomeMsg)
   const [dropBackground, setDropBackground] = useState("#37474fff")
   const [dropBorder, setDropBorder] = useState()
-  const [loading, setLoading] = useState(false)
-  const [showFiles, setShowFiles] = useState(false)
-  const [showURLs, setShowURLs] = useState(false)
-  const [showProgress, setShowProgress] = useState(false)
-  const [files, setFiles] = useState([])
-  const [urls, setURLs] = useState([])
   const [fetching, setFetching] = useState(false)
-  const [disableDrop, setDisableDrop] = useState(false)
+  const [files, setFiles] = useState([])
+  const [loading, setLoading] = useState(false)
   const [progress, setProgress] = useState(10)
-  const [user, setUser] = useState(window.sessionStorage.userData ? JSON.parse(window.sessionStorage.userData) : null);
-  const [topicId, setTopicId] = useState()
   const [providedContext, setProvidedContext] = useState("")
+  const [showFiles, setShowFiles] = useState(false)
+  const [showProgress, setShowProgress] = useState(false)
+  const [showURLs, setShowURLs] = useState(false)
+  const [topicId, setTopicId] = useState()
+  const [urls, setURLs] = useState([])
+  const [user, setUser] = useState(window.sessionStorage.userData ? JSON.parse(window.sessionStorage.userData) : null);
+
 
   const handleLoginSuccess = useCallback(userData => {
     setUser(userData);
@@ -43,21 +37,21 @@ function LandingPage() {
     //TODO: check if we want to fetch user-specific data
   }, [setUser]);
 
-  function showError(msg=null, clear=false){
-    if(clear){
+  const showError = (msg = null, clear = false) => {
+    if (clear) {
       setDropBackground("#37474fff")
-      setDropBorder("#eeeeee")
+      setDropBorder()
       setDropMsg(welcomeMsg)
       return
     }
-    if(msg){
+    if (msg) {
       setDropMsg(msg)
       setFiles([])
     }
     else {
       msg = dropMsg
     }
-    
+
     setShowFiles(false)
     setShowProgress(false)
     setDropBackground("#ff000045")
@@ -68,15 +62,15 @@ function LandingPage() {
     // console.log("Error: " + msg)
   }
 
-  function processTopic(){
-      
-    function topicProcessError(res){
+  const processTopic = () => {
+
+    function topicProcessError(res) {
       // display error msg
-      showError("Topic (id = "+topicId+") processing failed. Please try again later.")
+      showError("Topic (id = " + topicId + ") processing failed. Please try again later.")
       console.log("Topic processing failed: [" + res.status + "] (" + res.message + ")")
     }
 
-    async function waitTopicProcess(res){
+    async function waitTopicProcess(res) {
       var nextProgress = progress
       const topic = res.body
       // const topicId = Object.keys(res.body)[0]
@@ -84,14 +78,14 @@ function LandingPage() {
       const checkStatus = (res) => {
         try {
           var p = Number(res.text)
-          if(p === -1){
+          if (p === -1) {
             throw new Error("Could not read metadata")
           }
           setProgress(p)
           nextProgress = p
           console.log("Topic processing progress=" + p)
         }
-        catch(e){
+        catch (e) {
           console.log("Error retrieving processing progress: " + e.message)
           setProgress(0)
           nextProgress = -1
@@ -100,21 +94,21 @@ function LandingPage() {
       }
 
       // request progress and wait for topic to be processed
-      while(nextProgress >= 0 && nextProgress < 100){
+      while (nextProgress >= 0 && nextProgress < 100) {
         // request synchronously to check progress
         await request.post("/check").send(topic).then((res) => checkStatus(res))
       }
 
       // send user to topic breakdown page
-      if(nextProgress === 100){
+      if (nextProgress === 100) {
         setDropMsg("Topic processed successfully")
         // setLoading(false)
-        window.location.href = topicURL 
+        window.location.href = topicURL
       }
     }
 
     // all files completed
-    if(files.filter((f) => f.completed).length === files.length){
+    if (files.filter((f) => f.completed).length === files.length) {
       console.log("Upload complete, processing files...")
 
       setDropMsg("Processing content...")
@@ -126,58 +120,58 @@ function LandingPage() {
 
       // create request to process content
       request.post("/process")
-        .send({files: files})
-        .send({topicId: topicId})
-        .send({userId: user.id})
-        .send({providedContext: providedContext})
+        .send({ files: files })
+        .send({ topicId: topicId })
+        .send({ userId: user.id })
+        .send({ providedContext: providedContext })
         .then(waitTopicProcess, topicProcessError);
     }
   }
 
-  function onDropAccepted(acceptedFiles){
-    console.log("User dropped accepted files = " + acceptedFiles.length + " state.files = " + files.length) 
+  const onDropAccepted = (acceptedFiles) => {
+    console.log("User dropped accepted files = " + acceptedFiles.length + " state.files = " + files.length)
 
     showError("", true)
     setFiles(files.concat(acceptedFiles))
-    setLoading(true)
-    
-    function uploadSuccess(res){
+    // setLoading(true)
+
+    function uploadSuccess(res) {
       console.log("Upload success: " + res.req._data.get('key') + " [" + res.status + "]")
 
       const newFiles = acceptedFiles.map((f, i) => {
-        if(f.name === res.req._data.get('key')){
+        if (f.name === res.req._data.get('key')) {
           f.completed = true
         }
         return f
       });
 
-      if(newFiles.filter((f) => f.completed).length === newFiles.length){
+      if (newFiles.filter((f) => f.completed).length === newFiles.length) {
         // all current files uploaded
         setLoading(false)
         setDropMsg("Add more files or hit 'ANALYZE'")
       }
     }
 
-    function uploadError(res){
+    function uploadError(res) {
       // display error msg
       showError("Oops, upload failed. Please try again later.")
       console.log("Upload failed: " + res.req._data.get('key') + " [" + res.status + "] (" + res.message + ")")
     }
 
-    function reqSuccess(res){
+    function reqSuccess(res) {
       setDropMsg("Signed request success, uploading files...")
       setShowFiles(true)
-      setShowProgress(false)
+      setShowProgress(true)
       // setDisableDrop(true)
       console.log(dropMsg)
 
       // upload files directly to s3
-      JSON.parse(res.text).forEach(file => { 
+      JSON.parse(res.text).forEach(file => {
         console.log("Uploading " + file.data.fields.key)
-        
+
         // post it as form data
         var postData = new FormData();
-        for(var key in file.data.fields){
+        for (var key in file.data.fields) {
           postData.append(key, file.data.fields[key]);
         }
         postData.append('file', acceptedFiles.filter((f) => f.name === file.data.fields.key)[0])
@@ -187,40 +181,40 @@ function LandingPage() {
         req.then(uploadSuccess, uploadError);
       })
     }
-    
-    function reqError(res){
+
+    function reqError(res) {
       // display error msg
       showError("Oops, upload failed. Please try again later.")
       console.log("Signed request failed: " + res.status + " (" + res.message + ")")
     }
 
-    function getSignedRequest(res){
-      if(!topicId) {
+    function getSignedRequest(res) {
+      if (!topicId) {
         setTopicId(res.body.topicId)
       }
 
       // get signed request to S3 service
       const fileArgs = []
       acceptedFiles.forEach(file => {
-        if(!file.completed)
-          fileArgs.push({"file": file.name, "type": file.type})
+        if (!file.completed)
+          fileArgs.push({ "file": file.name, "type": file.type })
       })
-      
+
       request
         .post('/sign_s3')
         .send(fileArgs)
-        .send({"topic_id": topicId || res.body.topicId})
+        .send({ "topic_id": topicId || res.body.topicId })
         .set('Accept', 'application/json')
         .then(reqSuccess, reqError)
     }
 
     // create new topic for the content
-    if(!topicId){
+    if (!topicId) {
       request
-      .put('/topic/' + user.id)
-      .send({'title': 'Topic template'})
-      .send({'description': 'This is a new topic'})
-      .then(getSignedRequest, () => showError("Oops, failed to create topic."))
+        .put('/topic/' + user.id)
+        .send({ 'title': 'Topic template' })
+        .send({ 'description': 'This is a new topic' })
+        .then(getSignedRequest, () => showError("Oops, failed to create topic."))
     }
     else {
       getSignedRequest()
@@ -229,11 +223,11 @@ function LandingPage() {
 
   const handleContextInput = (event) => {
     setProvidedContext(event.target.value);
-  };
+  }
 
   const handleURLInput = (event) => {
-    if(document.getElementById('input-url-text').value){
-      const url = {"url": document.getElementById('input-url-text').value, "metadata": ""}
+    if (document.getElementById('input-url-text').value) {
+      const url = { "url": document.getElementById('input-url-text').value, "metadata": "" }
 
       const metaSuccess = (res) => {
         url['metadata'] = res.body
@@ -250,16 +244,16 @@ function LandingPage() {
       }
 
       request
-      .post('/fetch_url')
-      .send({"url": document.getElementById('input-url-text').value})
-      .set('Accept', 'application/json')
-      .then(metaSuccess, metaError)
+        .post('/fetch_url')
+        .send({ "url": document.getElementById('input-url-text').value })
+        .set('Accept', 'application/json')
+        .then(metaSuccess, metaError)
 
       document.getElementById('input-url-text').value = ""
       document.getElementById('input-url-help-msg').innerText = ""
       setFetching(true)
     }
-  };
+  }
 
   const openInNewTab = (url) => {
     const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
@@ -273,7 +267,7 @@ function LandingPage() {
 
   const urlCards = urls.map((u) => (
     <Card variant="dark" className="Landing-url-card">
-      <Card.Img className="Landing-url-card-img" onClick={() => openInNewTab(u.metadata['og:url'])} variant="top" src={u.metadata['og:image'] || './placeholder.png'} style={u.metadata['og:image'] ? {opacity: '1'} : {opacity: '0.5'}} alt="Website image or cover" />
+      <Card.Img className="Landing-url-card-img" onClick={() => openInNewTab(u.metadata['og:url'])} variant="top" src={u.metadata['og:image'] || './placeholder.png'} style={u.metadata['og:image'] ? { opacity: '1' } : { opacity: '0.5' }} alt="Website image or cover" />
       <Card.Body>
         <Card.Title className="Landing-url-card-title">{u.metadata['og:site_name']}</Card.Title>
         <Card.Text>
@@ -282,86 +276,86 @@ function LandingPage() {
         <Button className="Landing-url-card-button" onClick={() => removeCard(u.metadata['og:url'])} variant="secondary">Remove</Button>
       </Card.Body>
       <Card.Footer>
-          <small onClick={() => openInNewTab(u.metadata['og:url'])} className="text-muted"><i>{u.metadata['og:url']}</i></small>
+        <small onClick={() => openInNewTab(u.metadata['og:url'])} className="text-muted"><i>{u.metadata['og:url']}</i></small>
       </Card.Footer>
     </Card>
   ));
 
   return (
-      <div className="Landing d-flex flex-column">
-        <GoogleOAuthProvider clientId="929889600149-2qik7i9dn76tr2lu78bc9m05ns27kmag.apps.googleusercontent.com">
-          <NavBar userData={user} setUser={handleLoginSuccess}/>
+    <div className="Landing d-flex flex-column">
+      <GoogleOAuthProvider clientId="929889600149-2qik7i9dn76tr2lu78bc9m05ns27kmag.apps.googleusercontent.com">
+        <NavBar userData={user} setUser={handleLoginSuccess} />
 
-          <div className="Landing-body">
-            <main>
-              <div className="Landing-header-top">
-                  {loading ? <img src={logo} className="Landing-logo" alt="logo" /> : <img src={logo} className="Landing-logo-static" alt="logo" />}
-              </div>
-                {user && (
-                  <Form.Group className="Landing-input-group mb-3" id="input-form-group">
-                    <div className="Landing-drop">
-                    <StyledDropzone
-                      noKeyboard={disableDrop}
-                      noClick={disableDrop}
-                      noDrag={disableDrop}
-                      onDropAccepted={onDropAccepted}
-                      msg={dropMsg}
-                      showFiles={showFiles}
-                      showProgress={showProgress}
-                      background={dropBackground}
-                      border={dropBorder}
-                      progress={progress}
-                      files={files}
-                    />
+        <div className="Landing-body">
+          <main>
+            <div className="Landing-header-top">
+              {loading ? <img src={logo} className="Landing-logo" alt="logo" /> : <img src={logo} className="Landing-logo-static" alt="logo" />}
+            </div>
+            {user && (
+              <Form.Group className="Landing-input-group mb-3" id="input-form-group">
+                <div className="Landing-drop">
+                  <StyledDropzone
+                    noKeyboard={disableDrop}
+                    noClick={disableDrop}
+                    noDrag={disableDrop}
+                    onDropAccepted={onDropAccepted}
+                    msg={dropMsg}
+                    showFiles={showFiles}
+                    showProgress={showProgress}
+                    background={dropBackground}
+                    border={dropBorder}
+                    progress={progress}
+                    files={files}
+                  />
+                </div>
+                {!loading && (
+                  <>
+                    {/* <Form.Text id="landing-input-or-label" muted>
+                      AND/OR
+                    </Form.Text> */}
+                    <div>
+                      <InputGroup className="Landing-input-url mb-3">
+                        <InputGroup.Text id="input-url-label">WWW</InputGroup.Text>
+                        <Form.Control id="input-url-text" aria-label="Add an URL to be verified" aria-describedby="input-url-help-msg" />
+                        <Button
+                          id="input-url-button"
+                          variant="dark"
+                          onClick={!fetching ? handleURLInput : null}
+                          disabled={fetching}
+
+                        >{fetching ? 'Loading...' : 'Add URL'}</Button>
+                      </InputGroup>
+                      <Form.Text id="input-url-help-msg" muted />
                     </div>
-                    {!loading && (
-                      <>
-                        <Form.Text id="landing-input-or-label" muted>
-                          AND/OR
-                        </Form.Text>
-                        <div>
-                          <InputGroup className="Landing-input-url mb-3">
-                                <InputGroup.Text id="input-url-label">WWW</InputGroup.Text>
-                                <Form.Control id="input-url-text" aria-label="Add an URL to be verified" aria-describedby="input-url-help-msg"/>
-                                <Button 
-                                  id="input-url-button" 
-                                  variant="dark"
-                                  onClick={!fetching ? handleURLInput : null}
-                                  disabled={fetching}
-
-                                >{fetching ? 'Loading...' : 'Add URL'}</Button>
-                          </InputGroup>
-                          <Form.Text id="input-url-help-msg" muted/>
-                        </div>
-                        <br style={{clear: "both"}} />
-                      </>
-                    )}
-                    
-                    {((showFiles && files.length > 0) || (showURLs && urls.length > 0)) && (
-                      <>
-                        <div className="Landing-url-card-container">{urlCards}</div>
-                        <div className="Landing-input">
-                          <Form.Label><b><i>Optional</i></b>: Claims or additional information about the files and URLs you want to fact-check</Form.Label>
-                          <Form.Control id="input-context" as="textarea" onChange={handleContextInput} placeholder='e.g. I came across this viral video claiming a new "turbo diet"' rows={3} />
-                        </div>
-                        <Button 
-                                id="input-process-button" 
-                                variant="dark" 
-                                onClick={!loading ? processTopic : null}
-                                disabled={(loading || !(files && files.filter((f) => f.completed).length === files.length))}
-                        >ANALYZE</Button>
-                      </>
-                      
-                    )}
-                  </Form.Group>
+                    <br style={{ clear: "both" }} />
+                  </>
                 )}
-            </main>
-            <footer>
 
-            </footer>
-          </div>
-        </GoogleOAuthProvider>
-      </div>
+                {((showFiles && files.length > 0) || (showURLs && urls.length > 0)) && (
+                  <>
+                    <div className="Landing-url-card-container">{urlCards}</div>
+                    <div className="Landing-input">
+                      <Form.Label><b><i>Optional</i></b>: Claims or additional information about the files and URLs you want to fact-check</Form.Label>
+                      <Form.Control id="input-context" as="textarea" onChange={handleContextInput} placeholder='e.g. I came across this viral video claiming a new "turbo diet"' rows={3} />
+                    </div>
+                    <Button
+                      id="input-process-button"
+                      variant="dark"
+                      onClick={!loading ? processTopic : null}
+                      disabled={(loading || !(files && files.filter((f) => f.completed).length === files.length))}
+                    >ANALYZE</Button>
+                  </>
+
+                )}
+              </Form.Group>
+            )}
+          </main>
+          <footer>
+
+          </footer>
+        </div>
+      </GoogleOAuthProvider>
+    </div>
   );
 }
 
--- src/StyledDropzone.js
+++ src/StyledDropzone.js
@@ -1,11 +1,11 @@
-import {React} from 'react';
-import {useDropzone} from 'react-dropzone';
+import { React } from 'react';
+import { useDropzone } from 'react-dropzone';
 import styled from 'styled-components';
 import './LandingPage.css';
 
 
 const getBackgroundColor = (props) => {
-  if(props.background) {
+  if (props.background) {
     return props.background
   }
   return '#37474fff'
@@ -13,18 +13,18 @@ const getBackgroundColor = (props) => {
 
 const getColor = (props) => {
   if (props.border) {
-      return props.border;
+    return props.border;
   }
   if (props.isDragAccept) {
-      return '#00e676';
+    return '#00e676';
   }
   if (props.isDragReject) {
-      return '#ff1744';
+    return '#ff1744';
   }
   if (props.isFocused) {
-      return '#2196f3';
+    return '#2196f3';
   }
-  
+
   return '#eeeeee';
 }
 
@@ -54,37 +54,44 @@ function StyledDropzone(props) {
     isFocused,
     isDragAccept,
     isDragReject,
-    } = useDropzone({
-        accept: {
-        'video/mp4': ['.mp4'],
-        'image/jpeg': ['.jpg', '.jpeg'],
-        'image/png': ['.png'],
-        'audio/mpeg': ['.mp3', '.mpga', '.m4a'],
-        'audio/wav': ['.wav'],
-        'audio/webm': ['.weba']
-        },
-        onDropAccepted: props.onDropAccepted,
-        noClick: props.noClick,
-        noDrag: props.noDrag,
-        noKeyboard: props.noKeyboard
-    });
+  } = useDropzone({
+    accept: {
+      'video/mp4': ['.mp4'],
+      'image/jpeg': ['.jpg', '.jpeg'],
+      'image/png': ['.png'],
+      'audio/mpeg': ['.mp3', '.mpga', '.m4a'],
+      'audio/wav': ['.wav'],
+      'audio/webm': ['.weba']
+    },
+    onDropAccepted: props.onDropAccepted,
+    noClick: props.noClick,
+    noDrag: props.noDrag,
+    noKeyboard: props.noKeyboard
+  });
 
-    const files = props.files.map((file) => (
-      <div className="Landing-drop-item" key={file.path}>
-        {file.path} ({(file.size/1024/1024).toFixed(2)} MB)
-        <div className={file.completed?'done':'loader'}></div>
+  const files = props.files.map((file) => (
+    <div className="Landing-drop-item" key={file.path}>
+      <div className="Landing-drop-img">
+        {file.type.includes("video") ?
+          <video width="15%" muted>
+            <source src={URL.createObjectURL(file)} type={file.type} />
+          </video> : <img src={file.type.includes("image") ? URL.createObjectURL(file) : "./placeholder.png"} style={file.type.includes("image") ? { opacity: '1' } : { opacity: '0.5' }} alt="Content preview"></img>
+        }
       </div>
-    ));
-  
+      <div className="Landing-drop-item-desc">
+        {file.path} ({(file.size / 1024 / 1024).toFixed(2)} MB)
+        <div className={file.completed ? 'done' : 'loader'}></div>
+      </div>
+    </div>
+  ));
+
   return (
-    <Container border={props.border} background={props.background} {...getRootProps({isFocused, isDragAccept, isDragReject})}>
+    <Container border={props.border} background={props.background} {...getRootProps({ isFocused, isDragAccept, isDragReject })}>
       <input {...getInputProps()} />
-      <div className='Landing-drop-message'>{props.msg}</div>
-      {(props.showFiles && !props.showLoading) ? <aside>
-        <div>{files}</div>
-        {/* <div className='loader'></div> */}
-      </aside>:""}
-      {(props.showProgress && !props.showFiles) ? <div className='loader' style={{width: props.progress + "%", marginLeft : 0}}></div>:""}
+      <div className="Landing-drop-message">{props.msg}</div>
+      {(props.showFiles && !props.showLoading) ?
+        <div className="Landing-drop-files">{files}</div> : ""}
+      {(props.showProgress && !props.showFiles) ? <div className='loader' style={{ width: props.progress + "%", marginLeft: 0 }}></div> : ""}
     </Container>
   );
 }
